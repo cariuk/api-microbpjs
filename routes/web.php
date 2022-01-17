68d77995ed240ede33f0cd20ec1eb79da2a6f4d8
cariuk <hademopilie@gmail.com>
2022-01-17 14:37:00 +0800

    Change Log : Perbaikan Penambahaan Proxy Reverse

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+if (env("PROXY_REVERSE",false)){
+    $app_url = config("app.url");
+    if (!empty($app_url)) {
+        URL::forceRootUrl($app_url);
+        $schema = explode(':', $app_url)[0];
+        URL::forceScheme($schema);
+    }
+}
 
 Route::get('/', function () {
     return response()->json([
